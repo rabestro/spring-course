7ef189c53390c739a7d872bef3c264e2d9a84624
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-21 20:31:14 +0200

    Lesson 6. Dependency Injection (DI). Part 2.

--- src/main/java/com/rabestro/springcourse/MusicPlayer.java
+++ src/main/java/com/rabestro/springcourse/MusicPlayer.java
@@ -1,13 +1,52 @@
 package com.rabestro.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 	private Music music;
-	
+	private List<Music> musicList = new ArrayList<>();
+	private String name;
+	private int volume;
+	public void playMusicList() {
+		musicList.forEach(x->System.out.println(x.getSong()));
+	}
+	public List<Music> getMusicList() {
+		return musicList;
+	}
+
+	public void setMusicList(List<Music> musicList) {
+		this.musicList = musicList;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
 	// IoC
 	public MusicPlayer(Music music) {
 		this.music = music;
 	}
 	
+	public MusicPlayer() {
+	}
+
+	public void setMusic(Music music) {
+		this.music = music;
+	}
+
 	public void playMusic() {
 		System.out.println("Playing: " + music.getSong());
 	}
--- src/main/java/com/rabestro/springcourse/TestSpring.java
+++ src/main/java/com/rabestro/springcourse/TestSpring.java
@@ -9,6 +9,10 @@ public class TestSpring {
 
 		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		musicPlayer.playMusic();
+		System.out.println(musicPlayer.getName());
+		System.out.println(musicPlayer.getVolume());
+		
+		musicPlayer.playMusicList();
 		
 		context.close();
 	}
