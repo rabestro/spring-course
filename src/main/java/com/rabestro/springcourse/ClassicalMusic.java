431ae0b231a404778de65e5635a3c9a8f43dbe28
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-22 01:32:53 +0200

    Lesson 8. Bean Lifecycle. Init, Destroy and Factory methods.

--- src/main/java/com/rabestro/springcourse/ClassicalMusic.java
+++ src/main/java/com/rabestro/springcourse/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package com.rabestro.springcourse;
 
 public class ClassicalMusic implements Music {
+	private ClassicalMusic() {}
+	
+	public static ClassicalMusic getClassicalMusic() {
+		System.out.println("ClassicalMusic - Factory method");
+		return new ClassicalMusic();
+	}
+	
+	public void init() {
+		System.out.println("ClassicalMusic - Initialisation.");
+	}
+
+	public void destroy() {
+		System.out.println("ClassicalMusic - Destruction");
+	}
 
 	@Override
 	public String getSong() {
