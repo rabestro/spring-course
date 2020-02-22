715b016d24b32732e481f862492d59b3a12d52ee
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-22 02:51:30 +0200

    Added annotation @Qualifier

--- src/main/java/com/rabestro/springcourse/SimpleMusicPlayer.java
+++ src/main/java/com/rabestro/springcourse/SimpleMusicPlayer.java
@@ -1,14 +1,15 @@
 package com.rabestro.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleMusicPlayer {
-	private ClassicalMusic music;
+	private Music music;
 	
 	@Autowired
-	public SimpleMusicPlayer(ClassicalMusic music) {
+	public SimpleMusicPlayer(@Qualifier("rockMusic") Music music) {
 		this.music = music;
 	}
 	
